0ef1959b52819508d58529f3d1301adc279cbfb1
Nichkov Roman <nichkovroman@gmail.com>
2022-09-24 13:22:44 +0300

    feat: add slider in testimonials section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
 import * as customFunctions from './modules/functions.js'
 
 // vendors
+// swiper slider
+import Swiper, { Pagination } from 'swiper'
+
+const swiper = new Swiper('.swiper--testimonials ', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 4,
+    spaceBetween: 30,
+
+    modules: [Pagination],
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+    },
+})
