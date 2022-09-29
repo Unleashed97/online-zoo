effb4b94d72ff5e7323576d04833173aba83e749
Nichkov Roman <nichkovroman@gmail.com>
2022-09-30 01:17:29 +0300

    feat: add nav active item

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -48,3 +48,9 @@ export const testimonialsTextSwap = () => {
         }
     })
 }
+
+export const handleNavClick = () => {
+    const nav = document.querySelectorAll('.nav')
+
+    console.log(nav)
+}
